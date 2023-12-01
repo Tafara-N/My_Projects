648fa72795219aac8743d0f4afc28d47513bb6a0
Brendon45 <jejebrendon722@gmail.com>
2023-12-01 13:57:35 -0800

    for loop in python is cool

---
+++ practices/6-for_loop.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Prints each character in a string"""
+
+f_name = input("first name:")
+
+for c in f_name:
+    print(c)
+
+l_name = input("Surname:")
+
+for c in l_name:
+    if (c == "e"):
+        continue
+    print(c*3)
+else:
+    print("Finally loop finished executing.")
+# remember the else staement is part of the loop
+# so the else statement will not execute if you break out of the loop
+
+# using the for loop in the range function 
+
+for i in range(5):
+    print(i)
+
+# finding items with the for loop
+
+name = input("Name:")
+
+for f in name:
+    if (f.lower() == "c"):
+        print(f)
+        break
+else:
+    print("Couldn't find what you were looking for.")
