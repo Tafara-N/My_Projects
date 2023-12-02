c23e02b9f634dfbf62ed4b6f4c7aa2b64d2cd93a
Brendon45 <jejebrendon722@gmail.com>
2023-12-02 08:19:43 -0800

    working with dictionaries in python

---
+++ practices/7-dictnry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Using a for loop to access values in a dictionary"""
+mdict = {"maths": 78, "science": 96}
+# 1
+for i in mdict:
+    print(mdict[i])
+# 2
+#for subject in mdict.key():
+ #   print(subject)
+# 3
+#for subject in midct.value():
+ #   print(subject)
