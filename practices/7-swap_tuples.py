cc2e8cb7a05984500e98369d3aa26c14293bf59a
Brendon45 <jejebrendon722@gmail.com>
2023-12-02 07:45:31 -0800

    swaping numbers

---
+++ practices/7-swap_tuples.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Author - Brendon Jeje
+"""Swap two numbers"""
+a = 10
+b = 20
+# swap the values of a and b
+# b = 10, a = 20
+temp = a   # temp = 10
+a = b   # a = 20
+b = temp   # b = 10
+
+print(f"a ={a} and b  ={b}")
