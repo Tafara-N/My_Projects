95ca4c3400075ac1e208673ef79737276a0f7470
Brendon45 <jejebrendon722@gmail.com>
2023-12-08 06:34:48 -0800

    sorting

---
+++ 09-data_structures/1-sorting.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+myList = ["Calvin", 21, 5.9]
+names = ["Blessing", "Calvin", "trig", "dimbo"]
+nums = [4, 7, 1, 34, 10, 8]
+
+# to know the number of elements
+print(len(myList))
+print(len(names))
+print(len(nums))
+
+# to sort the elements
+print(sorted(nums))
