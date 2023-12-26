c5e6763ed6c4051430bf06a63c06514566f628b4
Brendon45 <jejebrendon722@gmail.com>
2023-12-26 08:49:27 -0800

    average

---
+++ practices/average.py
@@ -0,0 +1,9 @@
+# Write a program to find out the average of a set of integers
+count = int(input("Enter the count of numbers you want to find the average for: "))
+sum = 0
+for number in range(count):
+    num = int(input(f"Enter number {number + 1}: "))
+    sum += num
+
+average = sum / count
+print(f"The average is: {average}")
