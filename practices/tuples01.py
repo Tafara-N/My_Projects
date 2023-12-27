8f97cc9533e32edc89ac14f80aa5e312e13115fc
Brendon45 <jejebrendon722@gmail.com>
2023-12-27 10:13:13 -0800

    tuples

---
+++ practices/tuples01.py
@@ -0,0 +1,12 @@
+#†uple - an ordered and unchangeable list
+
+student = ("Tom", 16, "male")
+
+#for item in student:
+    # print(item)
+
+# print(student.count("Tom"))
+# print(student.index("male"))
+
+if 16 in student:
+    print("Yes, we got your age Tom")
