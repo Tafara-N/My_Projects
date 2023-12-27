1e70387ce36cd915ab9e060d0c9f49dc86a6d04c
Brendon45 <jejebrendon722@gmail.com>
2023-12-27 11:00:37 -0800

    a better tommorrow

---
+++ practices/writing_file.py
@@ -0,0 +1,7 @@
+# with open("text.txt", 'w') as file:
+#     file.write("Hello better tommorrow!")
+
+text = "Working tirelesly for a better tommorrow"
+
+with open("test.txt", 'w') as file:
+    file.write(text)
