b125d4a37ba6bd6b8403c20da84ced7c912ecdcd
Brendon45 <jejebrendon722@gmail.com>
2023-12-27 10:47:30 -0800

    Zimbabweans

---
+++ practices/variables02.py
@@ -0,0 +1,13 @@
+num1 = 23
+num2 = 40
+
+sum = num1 + num2
+sum = str(sum)
+
+print("The sum of 10 & 20 is: "+ str(10+20))
+print("The sum of num1 & num2 is: "+ sum)
+
+nationality = "Zimbabweans"
+code = 263
+print("Hello,", nationality)
+print("Your age is:", code)
