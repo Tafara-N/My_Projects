6f31d8f52a991cd29dab875e44997ea63ab680dd
Brendon45 <jejebrendon722@gmail.com>
2023-12-29 12:59:07 -0800

    joining dict

---
+++ practices01/joining.py
@@ -0,0 +1,12 @@
+# syntax -> seperator.join(iterable)
+
+list1 = ['T', 'e', 'n', 'd', 'a', 'i']
+# print(''.join(list1))
+# print(' '.join(list1))
+# print('_'.join(list1))
+
+list2 = ["My", "name", "is", "Tendai"]
+# print(" ".join(list2))
+
+dictionary_01 = {"name": "Tendai", "age":19}
+print(" and ".join(dictionary_01))
