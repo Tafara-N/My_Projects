8534d183ac4d505fb58f3b89474c9396bf2f67a1
Brendon45 <jejebrendon722@gmail.com>
2023-12-28 22:55:13 -0800

    list comprehension

---
+++ practices01/list_comprhsn01.py
@@ -0,0 +1,6 @@
+names_list = ["Brendon", "Simeon", "Jace","Tom", "Synclaire", "Jireh", "Samuel"]
+names_sublist = []
+for name in names_list:
+    if name[0] == 'S':
+        names_sublist.append(name)
+print(names_sublist)
