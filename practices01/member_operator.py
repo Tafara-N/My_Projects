db61b07b9a909475367fe27376d23fa104fcf02b
Brendon45 <jejebrendon722@gmail.com>
2023-12-28 23:56:50 -0800

    membership

---
+++ practices01/member_operator.py
@@ -0,0 +1,14 @@
+name = "Tendai"
+
+# The in operator
+# print("Tend" in name)
+# print("d" in name)
+# print("m" in name)
+
+# The not in operator
+# print("Tend" not in name)
+# print("n" not in name)
+# print("m" not in name)
+
+# The operators without a variable
+print("nd" in "Brendon")
