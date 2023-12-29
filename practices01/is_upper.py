c1b8f39ed955daa9f9fd79e8563c3b69dbcc1b8b
Brendon45 <jejebrendon722@gmail.com>
2023-12-29 12:55:04 -0800

    upper and lower cases

---
+++ practices01/is_upper.py
@@ -0,0 +1,11 @@
+# first_name = "BRENDON"
+# if first_name.isupper():
+#     print("Your name is in upper case")
+# else:
+#     print("Your name is in lower case")
+
+first_name = "Brendon"
+if first_name.isupper():
+    print("Your name is in upper case")
+else:
+    print("Your name is in lower case")
