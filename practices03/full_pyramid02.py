fa573f8f7a5dd68f8ce213f1e9807f74280cde28
Brendon45 <jejebrendon722@gmail.com>
2024-01-15 00:37:55 -0800

    full_pyramid leaving spaces to the left

---
+++ practices03/full_pyramid02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""A pyramid using for loops"""
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    for space in range(1, rows - i + 1):
+        print(end=' ')
+    for star in range(1, i + 1):
+        print("*", end='')
+    print('')
