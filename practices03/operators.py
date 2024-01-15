bdbbfe6e08f89ab503dd779eba1ff10004b295f0
Brendon45 <jejebrendon722@gmail.com>
2024-01-15 05:45:18 -0800

    operators

---
+++ practices03/operators.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#Logical operators (and, or, not)
+
+age = int(input("Enter your age: "))
+
+if age >= 18 and age <= 50:
+    print("You are a good fit for the job, you can work")
+else:
+    print("Your age does not allow you to work")
