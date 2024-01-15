fb3c7c5c0278d353465f94199cb40ea47c2fba1b
Brendon45 <jejebrendon722@gmail.com>
2024-01-15 05:09:45 -0800

    replacement

---
+++ practices03/replace.py
@@ -0,0 +1,5 @@
+# syntax -> string.replace(old_string, new_string, count)
+str = "My favourite name is Brendon"
+print(str.replace("Brendon", "Tendai"))
+print(str.replace(" ", "-", 2))
+print(str.replace(" ", "-"))
