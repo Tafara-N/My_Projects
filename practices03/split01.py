d49c7aad242703a950a61ee25284d8b5f44500bb
Brendon45 <jejebrendon722@gmail.com>
2024-01-15 05:54:13 -0800

    splits

---
+++ practices03/split01.py
@@ -0,0 +1,11 @@
+name = "             Brendon           "
+print(name.strip())
+
+last_name = "##########Jeje########"
+print(last_name.strip('#'))
+
+nick_name = " ####Tendai####"
+print(nick_name.strip('#'))
+
+full_name = "Brendon Jeje"
+print(full_name.strip("24hrP"))
