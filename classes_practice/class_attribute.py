7626faa84aca66d3c2a48ee5257c38be54333591
Brendon45 <jejebrendon722@gmail.com>
2024-02-02 13:34:23 -0800

    class attribute

---
+++ classes_practice/class_attribute.py
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+class Person:
+    
+    def __init__(self, name="", age=0):
+        self.name = name
+        self.age = age
+
+    ...
+
+person1 = Person("Rispo", 24)
+person2 = Person()
+person3 = Person()
+person3.name = "Tashinga"
+person3.age = 12
+
+# print(Person)
+# print(person1)
+# print(person2)
+# print(person3)
+
+# print(Person.__doc__)
+
+print(Person.__dict__)
+
+print(person1.__dict__)
+print(person2.__dict__)
+print(person3.__dict__)
+
+Person.name = "Dylan"
+print(person1.name)
+print(person2.name)
+print(Person.name)
