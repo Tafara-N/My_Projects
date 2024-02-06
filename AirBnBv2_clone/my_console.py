9892aae2fc32b883a9c455b4b6a0e7b471d076e6
Brendon45 <jejebrendon722@gmail.com>
2024-02-06 15:52:03 +0200

    the consoles

--- AirBnBv2_clone/my_console.py
+++ AirBnBv2_clone/my_console.py
@@ -6,15 +6,34 @@ class command(cmd.Cmd):
 
 	"""a simple command line interpreter"""
 
+	prompt = ">>>"
+
 	def do_greet(self, line):
-		print("Hello world")
+		"""Greet the name <person>"""
+		if line:	
+			print("Hello,", line)
+		else:
+			print("Hi")
+
+	def help_greet(self):
+		print('\n'.join(['greet [person]',]))
 
 	def do_EOF(self, line):
+		"""Exit the console gracefully"""
 		print()
 		return True
 
 	def default(self, line):
 		print(f"Unknown command: {line}")
+
+	def emptyline(self):
+		print("You have entered an empty line")
+
+	def do_quit(self, line):
+		return True
+
+	def help_quit(self, line):
+		print("Quit the console")
 
 if __name__ == '__main__':
 	command().cmdloop()
